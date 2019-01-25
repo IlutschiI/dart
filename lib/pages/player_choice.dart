c98150429190437de3dcf8ca118b3f0a8019e3a1
lukas <wicki96@gmail.com>
2019-01-25 22:40:07 +0100

    added choosing players

--- lib/pages/player_choice.dart
+++ lib/pages/player_choice.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:dart/model/player.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class PlayerChoice extends StatefulWidget {
@@ -8,15 +11,112 @@ class PlayerChoice extends StatefulWidget {
 
 class _PlayerChoiceState extends State<PlayerChoice> {
   List<Player> _players = List();
+  String newPlayerName = "";
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: mapPlayers());
+    var scrollController = ScrollController();
+    Timer(Duration(milliseconds: 100), () => scrollController.jumpTo(scrollController.position.maxScrollExtent));
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(5),
+        child: ListView(
+          controller: scrollController,
+          children: createPlayerWidgets(),
+          shrinkWrap: true,
+        ),
+      ),
+      bottomNavigationBar: Container(
+        child: RaisedButton(
+          onPressed: () {},
+          child: Text("Let's go!"),
+        ),
+      ),
+    );
   }
 
   List<Widget> mapPlayers() {
     return _players.map((p) {
-      return Container(child: Text(""));
+      return Container(
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: IconButton(
+                  icon: Icon(
+                    Icons.person,
+                    color: p.isSelected ? Colors.green : Colors.black,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      p.isSelected = !p.isSelected;
+                    });
+                  }),
+              flex: 1,
+            ),
+            Flexible(
+              child: Container(
+                  decoration: BoxDecoration(border: Border(bottom: BorderSide())),
+                  child: TextField(
+                    controller: TextEditingController(text: p.name),
+                    onChanged: (value) => p.name = value,
+                  )),
+              flex: 5,
+              fit: FlexFit.tight,
+            ),
+            Flexible(
+              child: IconButton(
+                  icon: Icon(Icons.delete_forever),
+                  onPressed: () {
+                    setState(() {
+                      _players.remove(p);
+                    });
+                  }),
+              flex: 1,
+            )
+          ],
+        ),
+      );
     }).toList();
+  }
+
+  List<Widget> createPlayerWidgets() {
+    List<Widget> players = mapPlayers();
+    players.add(createAddingPlayer());
+    return players;
+  }
+
+  Container createAddingPlayer() {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: Container(child: IconButton(icon: Icon(Icons.person), onPressed: () {})),
+          ),
+          Flexible(
+            child: Container(
+              child: TextField(
+                onChanged: (value) => newPlayerName = value,
+              ),
+            ),
+            flex: 5,
+          ),
+          Flexible(
+            child: Container(child: IconButton(icon: Icon(Icons.add), onPressed: addPlayer)),
+          )
+        ],
+      ),
+    );
+  }
+
+  void addPlayer() {
+    setState(() {
+      _players.add(new Player(newPlayerName));
+    });
+  }
+
+  deletePlayer(Player p) {
+    setState(() {
+      _players.remove(p);
+    });
   }
 }
